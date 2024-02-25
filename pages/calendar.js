4cd953227609a79cf1c06255f193150f8a262baf
Aryan <arysin163@gmail.com>
2024-02-24 21:05:26 -0500

    sample event added to fullcalendar

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,11 +1,16 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-
+// https://fullcalendar.io/docs/react
 export default function Calendar() {
   return (
     <FullCalendar
-      plugins={[ dayGridPlugin ]}
-      initialView="dayGridMonth"
-    />
+    plugins={[ dayGridPlugin ]}
+    initialView="dayGridMonth"
+    weekends={false}
+    events={[
+    { title: 'Maths', date: '2024-02-12' },
+    { title: 'Physics', date: '2024-04-02' }
+  ]}
+/>
   )
 }
