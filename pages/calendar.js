a9b9aa518e86df0c1337f8bde90dc18ad1ed71c8
sharma-rishi-130198 <43779891+sharma-rishi-130198@users.noreply.github.com>
2024-02-24 22:36:25 -0500

    Linked with Server for View Calender API

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,16 +1,36 @@
+import { useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import axios from 'axios'; // Import axios for making HTTP requests
 // https://fullcalendar.io/docs/react
 export default function Calendar() {
+
+  const [events, setEvents] = useState([]);
+  var username = 'rs13';
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.post('http://localhost:4200/getforuser',{username});
+        const eventData = response.data.data.map(subtask => ({
+          title: subtask.subtaskname,
+          date: new Date(subtask.subtaskdate).toISOString().split('T')[0] // Convert date to ISO format
+        }));
+        setEvents(eventData);
+      } catch (error) {
+        console.error('Error fetching events:', error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <FullCalendar
     plugins={[ dayGridPlugin ]}
     initialView="dayGridMonth"
     weekends={false}
-    events={[
-    { title: 'Maths', date: '2024-02-12' },
-    { title: 'Physics', date: '2024-04-02' }
-  ]}
+    events={events}
 />
   )
 }
